cf08eaabdcf238594b014c2beabad0ba9f452879
akhilendbohare <146517273+akhilendbohare@users.noreply.github.com>
2023-09-30 12:23:05 +0530

    Create avrge.py

---
+++ avrge.py
@@ -0,0 +1,8 @@
+a=int(input("enter the marks of stud1="))
+b=int(input("enter the marks of stud2="))
+c=int(input("enter the marks of stud3="))
+d=int(input("enter thr marks of stud4="))
+e=int(input("enter thr marks of stud5="))
+tot=a+b+c+d+e
+avge=tot/5
+print(avge)
